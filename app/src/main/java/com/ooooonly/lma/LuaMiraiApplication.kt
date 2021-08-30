bb5af86d4d81c04cd335be00c5eadd2cc0c5b53f
only52607 <ooooonly@foxmail.com>
2021-08-30 20:52:32 +0800

    Add Distribute enable

--- app/src/main/java/com/ooooonly/lma/LuaMiraiApplication.kt
+++ app/src/main/java/com/ooooonly/lma/LuaMiraiApplication.kt
@@ -1,6 +1,7 @@
 package com.ooooonly.lma
 
 import android.app.Application
+import android.util.Log
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -13,9 +14,12 @@ const val APP_SECRET = "c8b45524-322a-40ae-8c24-5d5cc8046f7b"
 class LuaMiraiApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        Distribute.setEnabled(true)
+        Distribute.setEnabledForDebuggableBuild(true)
         AppCenter.start(
             this, APP_SECRET,
             Analytics::class.java, Crashes::class.java, Distribute::class.java
         )
+        Log.d("Distribute", Distribute.isEnabled().toString())
     }
 }
