e27bd97e1b3e10a6ebaa3d4112793046b0634ce6
only52607 <ooooonly@foxmail.com>
2021-08-30 15:02:05 +0800

    Update BotParamsForm.kt

--- app/src/main/java/com/ooooonly/lma/ui/bot/edit/BotParamsForm.kt
+++ app/src/main/java/com/ooooonly/lma/ui/bot/edit/BotParamsForm.kt
@@ -1,11 +1,9 @@
 package com.ooooonly.lma.ui.bot.edit
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
